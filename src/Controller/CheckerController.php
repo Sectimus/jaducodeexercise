6bb20f7b6f4544f7e8661947c4b67fdf736a1d83
Sectimus <Sectimus@hotmail.co.uk>
2024-04-20 20:20:11 +0100

    Add interaction between controller and service for pangram checking

--- src/Controller/CheckerController.php
+++ src/Controller/CheckerController.php
@@ -17,6 +17,31 @@ class CheckerController extends AbstractController
         $this->checkerService = $checkerService;
     }
 
+    #[Route('/pangram/validate', name: 'check pangram', methods: ['POST'])]
+    public function pangram(Request $request): Response
+    {
+        $json = json_decode($request->getContent(), true);
+
+        if ($json === null || !isset($json['phrase'])) {
+            //failed to decode json, possible non-json payload provided
+            return $this->invalidContent("Please ensure you provide a valid JSON payload with a \"phrase\" key.");
+        }
+
+        $phrase = $json['phrase'];
+
+        if (!$this->checkerService->isPangram($phrase)) {
+            return new Response(
+                "The phrase: \"" . $phrase . "\" is NOT a pangram.",
+                Response::HTTP_OK
+            );
+        }
+
+        return new Response(
+            "The phrase: \"" . $phrase . "\" is a pangram.",
+            Response::HTTP_OK
+        );
+    }
+
     private function invalidContent(?string $message = null): Response
     {
         return new Response(
