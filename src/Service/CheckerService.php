4efbf0e85b4cdf16a52d535eaae9576cc9ca00fe
Sectimus <Sectimus@hotmail.co.uk>
2024-04-20 20:43:43 +0100

    Move all string perperations to a single function

--- src/Service/CheckerService.php
+++ src/Service/CheckerService.php
@@ -13,31 +13,39 @@ class CheckerService implements CheckerInterface
 
     public function isPalindrome(string $word): bool
     {
+        $word = $this->prep($word);
+
         return $word === strrev($word);
     }
 
     public function isAnagram(string $word, string $comparison): bool
     {
-        //remove any spaces from the word and comparison as they are not evaluated as part of an anagram
-        $word = str_replace(' ', '', $word);
-        $comparison = str_replace(' ', '', $comparison);
-
-        //drop all characters to lowercase to ensure case insensitivity
-        $word = strtolower($word);
-        $comparison = strtolower($comparison);
-
+        $word = $this->prep($word);
+        $comparison = $this->prep($comparison);
 
         return count_chars($word, 1) === count_chars($comparison, 1);
     }
 
     public function isPangram(string $phrase): bool
     {
-        /*remove any spaces from the word and comparison as they are not evaluated as part of a pangram
-          and drop all characters to lowercase to ensure case insensitivity*/
-        $phrase = strtolower(str_replace(' ', '', $phrase));
+        $phrase = $this->prep($phrase);
         $characterSet = str_split($phrase);
         $characterSet = array_unique($characterSet);
         $characterSet = array_intersect($characterSet, self::ALPHABET);
         return count($characterSet) === count(self::ALPHABET);
+    }
+
+    /**
+     * Prepares the provided string for usage in the service
+     */
+    private function prep(string $string): string
+    {
+        //remove any spaces from the word and comparison as they are not evaluated as part of an anagram
+        $string = str_replace(' ', '', $string);
+
+        //drop all characters to lowercase to ensure case insensitivity
+        $string = strtolower($string);
+
+        return $string;
     }
 }
