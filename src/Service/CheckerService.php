14ba9e70169d18f0a2f8d289052ef1da095fab57
Sectimus <Sectimus@hotmail.co.uk>
2024-04-20 19:37:12 +0100

    Add anagram checking functionality to the CheckerService and pass Unit tests

--- src/Service/CheckerService.php
+++ src/Service/CheckerService.php
@@ -13,7 +13,11 @@ class CheckerService implements CheckerInterface
 
     public function isAnagram(string $word, string $comparison): bool
     {
-        return false;
+        //remove any spaces from the word and comparison as they are not evaluated as part of an anagram
+        $word = str_replace(' ', '', $word);
+        $comparison = str_replace(' ', '', $comparison);
+
+        return count_chars($word, 1) === count_chars($comparison, 1);
     }
 
     public function isPangram(string $phrase): bool
