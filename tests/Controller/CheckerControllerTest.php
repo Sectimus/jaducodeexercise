6a7cb91e7c2f33d3e165442937abb276eb5077e2
Sectimus <Sectimus@hotmail.co.uk>
2024-04-21 16:37:22 +0100

    Add more integration tests and make mocking requests more generic

--- tests/Controller/CheckerControllerTest.php
+++ tests/Controller/CheckerControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @coversDefaultClass \App\Controller\CheckerController
@@ -24,11 +25,44 @@ final class CheckerControllerTest extends WebTestCase
      */
     public function testPalindromeValidationIntegration()
     {
-        $this->client->request("POST", "/palindrome/validate", [], [], [], json_encode(["word" => "anna"]));
+        // Valid palindrome
+        $response = $this->makeRequest("POST", "/palindrome/validate", ["word" => "anna"]);
 
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
-        $response = json_decode($this->client->getResponse()->getContent(), true);
-        $this->assertTrue($response["pass"]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertTrue($body["pass"]);
+
+        // Invalid palindrome
+        $response = $this->makeRequest("POST", "/palindrome/validate", ["word" => "Bark"]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
+    }
+
+    /**
+     * @group integration
+     * @covers ::palindrome
+     */
+    public function testPalindromeInvalidRequestIntegration()
+    {
+        // Invalid request method
+        $response = $this->makeRequest("GET", "/palindrome/validate", ["word" => "anna"]);
+        $this->assertEquals(405, $response->getStatusCode());
+
+        // Missing required JSON keys
+        $response = $this->makeRequest("POST", "/palindrome/validate", ["someword" => "anna"]);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
+
+        // Invalid request body
+        $response = $this->makeRequest("POST", "/palindrome/validate", "", false);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
     }
 
     /**
@@ -37,11 +71,45 @@ final class CheckerControllerTest extends WebTestCase
      */
     public function testAnagramValidationIntegration()
     {
-        $this->client->request("POST", "/anagram/validate", [], [], [], json_encode(["word" => "coalface", "comparison" => "cacao elf"]));
 
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
-        $response = json_decode($this->client->getResponse()->getContent(), true);
-        $this->assertTrue($response["pass"]);
+        // Valid anagram
+        $response = $this->makeRequest("POST", "/anagram/validate", ["word" => "coalface", "comparison" => "cacao elf"]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertTrue($body["pass"]);
+
+        // Invalid anagram
+        $response = $this->makeRequest("POST", "/anagram/validate", ["word" => "coalface", "comparison" => "dark elf"]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
+    }
+
+    /**
+     * @group integration
+     * @covers ::anagram
+     */
+    public function testAnagramInvalidRequestIntegration()
+    {
+        // Invalid request method
+        $response = $this->makeRequest("GET", "/anagram/validate", ["phrase" => "The quick brown fox jumps over the lazy dog"]);
+        $this->assertEquals(405, $response->getStatusCode());
+
+        // Missing required JSON keys
+        $response = $this->makeRequest("POST", "/anagram/validate", ["catchphrase" => "The quick brown fox jumps over the lazy dog"]);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
+
+        // Invalid request body
+        $response = $this->makeRequest("POST", "/anagram/validate", "", false);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
     }
 
     /**
@@ -50,10 +118,50 @@ final class CheckerControllerTest extends WebTestCase
      */
     public function testPangramValidationIntegration()
     {
-        $this->client->request("POST", "/pangram/validate", [], [], [], json_encode(["phrase" => "The quick brown fox jumps over the lazy dog"]));
 
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
-        $response = json_decode($this->client->getResponse()->getContent(), true);
-        $this->assertTrue($response["pass"]);
+        // Valid pangram
+        $response = $this->makeRequest("POST", "/pangram/validate", ["phrase" => "The quick brown fox jumps over the lazy dog"]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertTrue($body["pass"]);
+
+        // Invalid pangram
+        $response = $this->makeRequest("POST", "/pangram/validate", ["phrase" => "The British Broadcasting Corporation (BBC) is a British public service broadcaster."]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
+    }
+
+    /**
+     * @group integration
+     * @covers ::pangram
+     */
+    public function testPangramInvalidRequestIntegration()
+    {
+        // Invalid request method
+        $response = $this->makeRequest("GET", "/pangram/validate", ["phrase" => "The quick brown fox jumps over the lazy dog"]);
+        $this->assertEquals(405, $response->getStatusCode());
+
+        // Missing required JSON keys
+        $response = $this->makeRequest("POST", "/pangram/validate", ["catchphrase" => "The quick brown fox jumps over the lazy dog"]);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
+
+        // Invalid request body
+        $response = $this->makeRequest("POST", "/pangram/validate", "", false);
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $body = json_decode($response->getContent(), true);
+        $this->assertFalse($body["pass"]);
+    }
+
+    private function makeRequest(string $method, string $uri, $data, bool $parseJsonBody = true): Response
+    {
+        $this->client->request($method, $uri, [], [], [], $parseJsonBody ? json_encode($data) : $data);
+        return $this->client->getResponse();
     }
 }
